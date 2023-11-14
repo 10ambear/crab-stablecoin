c769c873dcffc46192396860620f259c5ed35854
Soulrealz <breekzstalkers@gmail.com>
2023-11-14 14:40:57 +0200

    add interfaces for cdp and government

---
+++ src/interfaces/ICDP.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+interface ICDP {
+
+    /**
+     * @dev Deposit the specified collateral into the caller's position.
+     * Only supported collateralToken's are allowed.
+     * 
+     * @param collateralToken the token to supply as collateral.
+     * @param amount the amount of collateralToken to provide.
+     */
+    function depositCollateral(address collateralToken, uint256 amount) external;
+
+    /**
+     * @dev Withdraw the specified collateral from the caller's position.
+     *
+     * @param collateralToken the token to withdraw from collateral.
+     * @param amount the amount of collateral to withdraw.
+     */
+    function withdrawCollateral(address collateralToken, uint256 amount) external;
+
+    /**
+     * @dev Borrow protocol stablecoins against the caller's collateral.
+     * 
+     * @notice The caller is not allowed to exceed the ltv ratio for their basket of collateral.
+     *
+     * @param amount the amount to borrow.
+     */
+    function borrow(uint256 amount) external;
+
+    /**
+     * @dev Repay protocol stablecoins from the caller's debt.
+     * 
+     * @param amount the amount to repay.
+     */
+    function repay(uint256 amount) external;
+
+}
